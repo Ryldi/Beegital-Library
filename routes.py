b3467a4b9dbbb1526e566949ca2da0c55838aaf1
Ryldi <reynaldi.adidarma@binus.ac.id>
2024-08-16 11:49:46 +0700

    chore: new database

--- routes.py
+++ routes.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'db_test'
+app.config['MYSQL_DB'] = 'beegital-library_db'
 
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['SESSION_TYPE'] = 'filesystem'
